aa32e05d4ed053e73159cf7f18f3092645524cf1
Gert Wohlgemuth <berlinguyinca@gmail.com>
2018-06-12 13:04:55 -0700

    Added the first technical indicator wrappers

--- technical/indicators.py
+++ technical/indicators.py
@@ -2,3 +2,13 @@
 This file contains a collection of common indicators, which are based on third party or custom libraries
 
 """
+from pyti.chaikin_money_flow import chaikin_money_flow
+from pyti.accumulation_distribution import accumulation_distribution as acd
+
+
+def cmf(dataframe, period):
+    return chaikin_money_flow(dataframe['close'], dataframe['high'], dataframe['low'], dataframe['volume'], period)
+
+
+def accumulation_distribution(dataframe):
+    return acd(dataframe['close'], dataframe['high'], dataframe['low'], dataframe['volume'])
