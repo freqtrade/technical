d2d5e7f30386b5f02dd0e9008db01704609af44e
Gert Wohlgemuth <berlinguyinca@gmail.com>
2018-06-12 12:57:53 -0700

    setup initial enviornment

---
+++ setup.py
@@ -0,0 +1,31 @@
+from sys import version_info
+from setuptools import setup
+
+if version_info.major == 3 and version_info.minor < 6 or \
+        version_info.major < 3:
+    print('Your Python interpreter must be 3.6 or greater!')
+    exit(1)
+
+setup(name='technical',
+      version=1.0,
+      description='Technical Indicators',
+      url='https://github.com/berlinguyinca/technical',
+      author='Gert Wohlgemuth and Contributors',
+      author_email='berlinguyinca@gmail.com',
+      license='GPLv3',
+      packages=['technical'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'pytest-mock', 'pytest-cov'],
+      install_requires=[
+          'TA-Lib',
+          'pyti',
+          'pandas'
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+          'Programming Language :: Python :: 3.6',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Topic :: Office/Business :: Financial :: Investment',
+          'Intended Audience :: Science/Research',
+      ])
---
+++ technical/indicators.py
@@ -0,0 +1,4 @@
+"""
+This file contains a collection of common indicators, which are based on third party or custom libraries
+
+"""
