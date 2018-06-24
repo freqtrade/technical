f689cf45605df2aa25c6f5864074c0ce90ebeae8
creslinux <creslin@gmail.com>
2018-06-24 19:52:45 +0000

    vfi

--- technical/indicators.py
+++ technical/indicators.py
@@ -4,7 +4,8 @@ This file contains a collection of common indicators, which are based on third p
 """
 from numpy.core.records import ndarray
 from pandas import Series, DataFrame
-
+from math import log
+from statistics import stdev
 
 def aroon(dataframe, period=25, field='close', colum_prefix="aroon") -> DataFrame:
     from pyti.aroon import aroon_up as up
@@ -74,3 +75,117 @@ def cci(dataframe, period) -> ndarray:
     from pyti.commodity_channel_index import commodity_channel_index
 
     return commodity_channel_index(dataframe['close'], dataframe['high'], dataframe['low'], period)
+
+
+def vfi(dataframe, length=130, coef=0.2, vcoef=2.5, signalLength=5, smoothVFI=False):
+    """
+    Volume Flow Indicator conversion
+
+    Author: creslinux, June 2018 - Python
+    Original Author: Chris Moody, TradingView - Pinescript
+    To return vfi, vfima and histogram
+
+    A simplified interpretation of the VFI is:
+    * Values above zero indicate a bullish state and the crossing of the zero line is the trigger or buy signal.
+    * The strongest signal with all money flow indicators is of course divergence.
+    * A crossover of vfi > vfima is uptrend
+    * A crossunder of vfima > vfi is downtrend
+    * smoothVFI can be set to smooth for a cleaner plot to ease false signals
+    * histogram can be used against self -1 to check if upward or downward momentum
+
+
+    :param dataframe:
+    :param length: - VFI Length - 130 default
+    :param coef:  - price coef  - 0.2 default
+    :param vcoef: - volume coef  - 2.5 default
+    :param signalLength: - 5 default
+    :param smoothVFI:  bool - False detault
+    :return: vfi, vfima, histogram
+    """
+
+    """"
+    Original Pinescript 
+    From: https://www.tradingview.com/script/MhlDpfdS-Volume-Flow-Indicator-LazyBear/
+    
+    length = input(130, title="VFI length")
+    coef = input(0.2)
+    vcoef = input(2.5, title="Max. vol. cutoff")
+    signalLength=input(5)
+    smoothVFI=input(false, type=bool)
+    
+    #### Conversion summary to python 
+      - ma(x,y) => smoothVFI ? sma(x,y) : x // Added as smoothVFI test on vfi
+    
+      - typical = hlc3  // Added to DF as HLC
+      - inter = log(typical) - log(typical[1]) // Added to DF as inter
+      - vinter = stdev(inter, 30) // Added to DF as vinter
+      - cutoff = coef * vinter * close // Added to DF as cutoff
+      - vave = sma(volume, length)[1] // Added to DF as vave
+      - vmax = vave * vcoef // Added to Df as vmax
+      - vc = iff(volume < vmax, volume, vmax) // Added np.where test, result in DF as vc
+      - mf = typical - typical[1] // Added into DF as mf - typical is hlc3
+      - vcp = iff(mf > cutoff, vc, iff(mf < -cutoff, -vc, 0)) // added in def vcp, in DF as vcp
+
+      - vfi = ma(sum(vcp, length) / vave, 3) // Added as DF vfi. Will sma vfi 3 if smoothVFI flag set
+      - vfima = ema(vfi, signalLength) // added to DF as vfima
+      - d = vfi-vfima // Added to df as histogram
+    
+    ### Pinscript plotout - nothing to do here for freqtrade.
+    plot(0, color=gray, style=3)
+    showHisto=input(false, type=bool)
+    plot(showHisto ? d : na, style=histogram, color=gray, linewidth=3, transp=50)
+    plot( vfima , title="EMA of vfi", color=orange)
+    plot( vfi, title="vfi", color=green,linewidth=2)
+    """
+    import talib as ta
+    from pyti.exponential_moving_average import exponential_moving_average as ema
+    from pyti.simple_moving_average import simple_moving_average as sma
+    from numpy import where
+
+    length = length
+    coef = coef
+    vcoef = vcoef
+    signalLength = signalLength
+    smoothVFI = smoothVFI
+    df=dataframe
+    #Add hlc3 and populate inter to the dataframe
+    df['hlc'] = ((df['high'] + df['low'] + df['close'])/3).astype(float)
+    df['hlc_log'] = df['hlc'].map(log)
+    df['hlc_log_1'] = df['hlc'].shift(+1).map(log)
+    df['inter'] =  df['hlc_log'] - df['hlc_log_1']
+    #df['inter'] = df['hlc'].map(log) - df['hlc'].shift(+1).map(log)
+    df['vinter'] = df['inter'].rolling(30).std(ddof=0)
+    df['cutoff'] = ( coef * df['vinter'] * df['close'] )
+    # Vave is to be calculated on volume of the past bar
+    df['vave'] = sma(df['volume'].shift(+1), length)
+    df['vmax'] =  df['vave'] * vcoef
+    df['vc'] = where((df['volume'] < df['vmax']), df['volume'], df['vmax'])
+    df['mf'] = df['hlc'] - df['hlc'].shift(+1)
+    # more logic for vcp, so create a def and df.apply it
+    def vcp(x):
+        if x['mf'] > x['cutoff']:
+            return x['vc']
+        elif x['mf'] < -(x['cutoff']):
+            return -(x['vc'])
+        else:
+            return 0
+    df['vcp'] = df.apply(vcp, axis=1)
+    # vfi has a smooth option passed over def call, sma if set
+    df['vfi'] = (df['vcp'] + length) / df['vave']
+    if smoothVFI == True :
+        df['vfi'] = sma(df['vfi'], 3)
+    df['vfima'] = ta.EMA( df['vfi'] , 5)
+    df['histogram']  = df['vfi'] - df['vfima']
+
+    # df['inter'] = df['hlc_log'] - df['hlc_log'].shift(+1)
+    # df['v2'] =  df['volume'] - df['volume'].shift(+1)
+    # df['c2'] = df['close'] - df['close'].shift(+1)
+    # print(df.dtypes)
+    # lets get last row and 2nd last row
+    # l2=len(df)-2
+
+    # import pandas as pd
+    # pd.set_option('display.max_rows', 5000)
+    #print(df.loc[(l2 - 40):l1,['date', 'close', 'vinter', \
+    #df.to_csv("vfi.csv")
+    return df
