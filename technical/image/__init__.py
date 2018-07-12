23c127a6c5f7caf63706571f17e1734602936caa
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2018-07-12 11:54:05 -0700

    added laguire indicator

---
+++ technical/image/__init__.py
@@ -0,0 +1,12 @@
+
+def get_coin_in_image(image) -> str :
+    """
+    this method extracts a given highlighted coin in the provided image
+    using the tesseract library
+    :param image:
+    :return:
+    """
+
+    #1. if circle
+
+    #2. if arrows
--- technical/indicators.py
+++ technical/indicators.py
@@ -7,7 +7,6 @@ from pandas import Series, DataFrame
 from math import log
 
 
-
 def aroon(dataframe, period=25, field='close', colum_prefix="aroon") -> DataFrame:
     from pyti.aroon import aroon_up as up
     from pyti.aroon import aroon_down as down
@@ -198,7 +197,7 @@ def vfi(dataframe, length=130, coef=0.2, vcoef=2.5, signalLength=5, smoothVFI=Fa
     return df['vfi'], df['vfima'], df['vfi_hist']
 
 
-def mmar(self, dataframe, matype="EMA", src="close", debug=False):
+def mmar(dataframe, matype="EMA", src="close", debug=False):
     """
     Madrid Moving Average Ribbon
 
@@ -319,7 +318,7 @@ def mmar(self, dataframe, matype="EMA", src="close", debug=False):
     df["ma70"] = ma(df[src], 70)
     df['ma70l'] = df['ma70'].shift(+1)
     df["ma80"] = ma(df[src], 80)
-    df['ma80l'] = df['ma80'].shift(+1)
+    df['ma80ll'] = df['ma80'].shift(+1)
     df["ma90"] = ma(df[src], 90)
     df['ma90l'] = df['ma90'].shift(+1)
     df["ma100"] = ma(df[src], 100)
@@ -405,7 +404,7 @@ def mmar(self, dataframe, matype="EMA", src="close", debug=False):
 
     if debug:
         from pandas import set_option
-        set_option('display.max_rows', 2000)
+        set_option('display.max_rows', 10)
         print(df[["date", "leadMA",
                   "ma05", "ma05l", "ma05_c",
                   "ma10", "ma10l", "ma10_c",
@@ -437,7 +436,7 @@ def mmar(self, dataframe, matype="EMA", src="close", debug=False):
            df['ma80_c'], df['ma90_c']
 
 
-def madrid_sqz(self, datafame, length=34, src='close', ref=13, sqzLen=5):
+def madrid_sqz(datafame, length=34, src='close', ref=13, sqzLen=5):
     """
     Squeeze Madrid Indicator
 
@@ -554,7 +553,7 @@ def madrid_sqz(self, datafame, length=34, src='close', ref=13, sqzLen=5):
     return df['sqz_cma_c'], df['sqz_rma_c'], df['sqz_sma_c']
 
 
-def stc(self, dataframe, fast=23, slow=50, length=10):
+def stc(dataframe, fast=23, slow=50, length=10):
     import pandas as pd
     # First, the 23-period and the 50-period EMA and the MACD values are calculated:
     # EMA1 = EMA (Close, Short Length);
@@ -565,10 +564,101 @@ def stc(self, dataframe, fast=23, slow=50, length=10):
     # %D (MACD) = %DV (MACD, 10);
     # Schaff = 100 x (MACD – %K (MACD)) / (%D (MACD) – %K (MACD))
 
+    import talib.abstract as ta
+
     MACD = ta.EMA(dataframe, timeperiod=fast) - ta.EMA(dataframe, timeperiod=slow)
     STOK = ((MACD - MACD.rolling(window=length).min()) / (
-                MACD.rolling(window=length).max() - MACD.rolling(window=length).min())) * 100
+            MACD.rolling(window=length).max() - MACD.rolling(window=length).min())) * 100
     STOD = STOK.rolling(window=length).mean()
     dataframe['stc'] = 100 * (MACD - (STOK * MACD)) / ((STOD * MACD) - (STOK * MACD))
 
     return dataframe['stc']
+
+
+def laguerre(dataframe, gamma=0.75, smooth=1, debug=bool):
+    """
+    laguerre RSI
+    Author Creslin
+    Original Author: John Ehlers 1979
+
+
+    :param dataframe: df
+    :param gamma: Between 0 and 1, default 0.75
+    :param smooth: 1 is off. Valid values over 1 are alook back smooth for an ema
+    :param debug: Bool, prints to console
+    :return: Laguerre RSI:values 0 to +1
+    """
+    """
+    Laguerra RSI 
+    How to trade lrsi:  (TL, DR) buy on the flat 0, sell on the drop from top,
+    not when touch the top
+    http://systemtradersuccess.com/testing-laguerre-rsi/
+
+    http://www.davenewberg.com/Trading/TS_Code/Ehlers_Indicators/Laguerre_RSI.html
+    """
+    import talib as ta
+    import pandas as pd
+    ema = ta.EMA
+
+    df = dataframe
+    g = gamma
+    smooth = smooth
+    debug = debug
+    if debug:
+        from pandas import set_option
+        set_option('display.max_rows', 2000)
+        set_option('display.max_columns', 8)
+
+    """
+    Vectorised pandas or numpy calculations are not used
+    in Laguerre as L0 is self referencing.
+    Therefore we use an intertuples loop as next best option. 
+    """
+    lrsi_l = []
+    L0, L1, L2, L3 = 0.0, 0.0, 0.0, 0.0
+    for row in df.itertuples(index=True, name='lrsi'):
+        """ Original Pine Logic  Block1
+        p = close
+        L0 = ((1 - g)*p)+(g*nz(L0[1]))
+        L1 = (-g*L0)+nz(L0[1])+(g*nz(L1[1]))
+        L2 = (-g*L1)+nz(L1[1])+(g*nz(L2[1]))
+        L3 = (-g*L2)+nz(L2[1])+(g*nz(L3[1])) 
+        """
+        # Feed back loop
+        L0_1, L1_1, L2_1, L3_1 = L0, L1, L2, L3
+
+        L0 = (1 - g) * row.close + g * L0_1
+        L1 = -g * L0 + L0_1 + g * L1_1
+        L2 = -g * L1 + L1_1 + g * L2_1
+        L3 = -g * L2 + L2_1 + g * L3_1
+
+        """ Original Pinescript Block 2 
+        cu=(L0 > L1? L0 - L1: 0) + (L1 > L2? L1 - L2: 0) + (L2 > L3? L2 - L3: 0)
+        cd=(L0 < L1? L1 - L0: 0) + (L1 < L2? L2 - L1: 0) + (L2 < L3? L3 - L2: 0)
+        """
+        cu = 0.0
+        cd = 0.0
+        if (L0 >= L1):
+            cu = L0 - L1
+        else:
+            cd = L1 - L0
+
+        if (L1 >= L2):
+            cu = cu + L1 - L2
+        else:
+            cd = cd + L2 - L1
+
+        if (L2 >= L3):
+            cu = cu + L2 - L3
+        else:
+            cd = cd + L3 - L2
+
+        """Original Pinescript  Block 3 
+        lrsi=ema((cu+cd==0? -1: cu+cd)==-1? 0: (cu/(cu+cd==0? -1: cu+cd)), smooth)
+        """
+        if (cu + cd) != 0:
+            lrsi_l.append(cu / (cu + cd))
+        else:
+            lrsi_l.append(0)
+
+    return lrsi_l
