e4eaef8fcc799fb04d784d3e76f581e35cc6765d
Gert Wohlgemuth <berlinguyinca@gmail.com>
2018-07-29 15:07:21 -0700

    removed tensorflow/keras for now

--- setup.py
+++ setup.py
@@ -22,9 +22,7 @@ setup(name='technical',
           'pandas',
           'simplejson',
           'arrow',
-          'SQLAlchemy',
-          'keras',
-          'tensorflow'
+          'SQLAlchemy'
       ],
       include_package_data=True,
       zip_safe=False,
