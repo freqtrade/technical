44f577f87eb6b219bd8cb98c7997ca8574d8279f
Pauldesco <63605804+Pauldesco@users.noreply.github.com>
2020-05-31 15:27:44 +0200

    Improve CMF
    This implementation would give performance boosts avoiding loop from pyti implementation with pure vectorized pandas calculations.

--- technical/indicators.py
+++ technical/indicators.py
@@ -175,11 +175,11 @@ def accumulation_distribution(dataframe) -> ndarray:
 # OBV                  On Balance Volume
 
 # Other Volume Indicator Functions
-def cmf(dataframe, period=14) -> ndarray:
-    from pyti.chaikin_money_flow import chaikin_money_flow
-
-    return chaikin_money_flow(dataframe['close'], dataframe['high'], dataframe['low'], dataframe['volume'], period)
-
+def chaikin_money_flow(dataframe, period=21):
+    mfm =((dataframe['close'] - dataframe['low']) - (dataframe['high'] - dataframe['close'])) / (dataframe['high'] - dataframe['low'])
+    mfv = mfm * dataframe['volume']
+    cmf = mfv.rolling(period).sum() / dataframe['volume'].rolling(period).sum()
+    return cmf
 
 ########################################
 #
