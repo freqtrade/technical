06906050b832ea784fddf0d3a45c342aefb01d64
boschje <michielbosch1971@gmail.com>
2021-02-19 13:28:33 +0700

    Improve calculation speed for Heikin Ashi
    - List comprehension method is significantly faster as a for loop
    - ha_open calculation based on: https://stackoverflow.com/a/55110393
    - As discussed in: 
    https://github.com/freqtrade/technical/pull/154#discussion_r578285877

--- technical/candles.py
+++ technical/candles.py
@@ -4,15 +4,15 @@ from scipy.ndimage import shift
 
 
 def heikinashi(bars):
-    """
+"""
     Heikin Ashi calculation: https://school.stockcharts.com/doku.php?id=chart_analysis:heikin_ashi
 
     ha_open calculation based on: https://stackoverflow.com/a/55110393
+    ha_open = [ calculate first record ][ append remaining records with list comprehension method ]
     list comprehension method is significantly faster as a for loop
-    ha_open = [calculate first record][append remaining records with list comprehension method]
 
     result:
-    ha_open[0] = (bars.open[0] + bars.close[0]) / 2
+    ha_open[0] =  (bars.open[0] + bars.close[0]) / 2
     ha_open[1] = (ha_open[0] + ha_close[0]) / 2
     ...
     ha_open[last] = ha_open[len(bars)-1] + ha_close[len(bars)-1]) / 2
@@ -20,14 +20,14 @@ def heikinashi(bars):
 
     bars = bars.copy()
 
-    bars['ha_close']=bars[['open','high','low','close']].mean(axis=1)
+    bars['ha_close'] = bars[['open', 'high', 'low', 'close']].mean(axis=1)
 
     ha_open = [ (bars.open[0] + bars.close[0]) / 2 ]
     [ ha_open.append((ha_open[x] + bars.ha_close[x]) / 2) for x in range(0, len(bars)-1) ]
     bars['ha_open'] = ha_open
 
-    bars['ha_high']=bars[['ha_open','ha_close','high']].max(axis=1)
-    bars['ha_low']=bars[['ha_open','ha_close','low']].min(axis=1)
+    bars['ha_high'] = bars[['ha_open', 'ha_close', 'high']].max(axis=1)
+    bars['ha_low'] = bars[['ha_open', 'ha_close', 'low']].min(axis=1)
 
     result = pd.DataFrame(
         index=bars.index,
