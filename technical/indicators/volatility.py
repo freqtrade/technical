19890d26863163195a9d3e910cd62ab8a3bdda5d
Julien GAINZA <gainza.julien@gmail.com>
2022-01-28 13:55:05 +0100

    add Chopiness index

--- technical/indicators/volatility.py
+++ technical/indicators/volatility.py
@@ -1,15 +1,21 @@
 """
-Volatility indicator functoins
+Volatility indicator functions
 """
 from numpy.core.records import ndarray
+import numpy as np
+import pandas as pd
+
 
 from technical.vendor.qtpylib.indicators import atr  # noqa: F401
+
 
 ########################################
 #
 # Volatility Indicator Functions
 #
 
+########################################
+#
 # ATR                  Average True Range
 # Imported from qtpylib, which is a fast implementation.
 
@@ -26,3 +32,32 @@ def atr_percent(dataframe, period: int = 14) -> ndarray:
 
 # NATR                 Normalized Average True Range
 # TRANGE               True Range
+
+########################################
+#
+# Choppiness index
+# 
+#  theory https://www.tradingview.com/scripts/choppinessindex/
+#  slightly adapeted from https://medium.com/codex/detecting-ranging-and-trending-markets-with-choppiness-index-in-python-1942e6450b58
+
+def chopiness(dataframe, period: int = 14):
+    """
+
+    :param dataframe: Dataframe containing candle data
+    :param period: Period to use for chopiness calculation (defaults to 14)
+    :return: Float value from chopiness calculation :values 0 to +100
+    """
+
+    tr1 = pd.DataFrame(dataframe['high'] - dataframe['low']).rename(columns = {0:'tr1'})
+    tr2 = pd.DataFrame(abs(dataframe['high'] - dataframe['close'].shift(1))).rename(columns = {0:'tr2'})
+    tr3 = pd.DataFrame(abs(dataframe['low'] - dataframe['close'].shift(1))).rename(columns = {0:'tr3'})
+    frames = [tr1, tr2, tr3]
+
+    tr = pd.concat(frames, axis = 1, join = 'inner').dropna().max(axis = 1)
+    atr = tr.rolling(1).mean()
+    highh = dataframe['high'].rolling(period).max()
+    lowl = dataframe['low'].rolling(period).min()
+    ci = 100 * np.log10((atr.rolling(period).sum()) / (highh - lowl)) / np.log10(period)
+    return ci
+
+    
