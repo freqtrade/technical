ae6b1a736c9c9d9feb2f2be501351d1f2008250e
Matthias <xmatthias@outlook.com>
2022-02-09 19:21:57 +0100

    Fix formatting problems, move choppiness description to docstring

--- technical/indicators/volatility.py
+++ technical/indicators/volatility.py
@@ -1,13 +1,11 @@
 """
 Volatility indicator functions
 """
-from numpy.core.records import ndarray
 import numpy as np
 import pandas as pd
-
+from numpy.core.records import ndarray
 
 from technical.vendor.qtpylib.indicators import atr  # noqa: F401
-
 
 ########################################
 #
@@ -34,27 +32,27 @@ def atr_percent(dataframe, period: int = 14) -> ndarray:
 # TRANGE               True Range
 
 ########################################
-#
-# Choppiness index
-# 
-#  theory https://www.tradingview.com/scripts/choppinessindex/
-#  slightly adapted from https://medium.com/codex/detecting-ranging-and-trending-markets-with-choppiness-index-in-python-1942e6450b58
+
 
 def chopiness(dataframe, period: int = 14):
     """
+    Choppiness index
+    theory https://www.tradingview.com/scripts/choppinessindex/
+    slightly adapted from
+    https://medium.com/codex/detecting-ranging-and-trending-markets-with-choppiness-index-in-python-1942e6450b58
 
     :param dataframe: Dataframe containing candle data
     :param period: Period to use for chopiness calculation (defaults to 14)
-    :return: Float value from chopiness calculation :values 0 to +100
+    :return: Series containing chopiness calculation :values 0 to +100
     """
 
-    tr1 = dataframe['high'] - dataframe['low']
-    tr2 = abs(dataframe['high'] - dataframe['close'].shift(1))
-    tr3 = abs(dataframe['low'] - dataframe['close'].shift(1))
+    tr1 = dataframe["high"] - dataframe["low"]
+    tr2 = abs(dataframe["high"] - dataframe["close"].shift(1))
+    tr3 = abs(dataframe["low"] - dataframe["close"].shift(1))
 
-    tr = pd.concat([tr1, tr2, tr3], axis = 1, join = 'inner').dropna().max(axis = 1)
+    tr = pd.concat([tr1, tr2, tr3], axis=1, join="inner").dropna().max(axis=1)
     atr = tr.rolling(1).mean()
-    highh = dataframe['high'].rolling(period).max()
-    lowl = dataframe['low'].rolling(period).min()
+    highh = dataframe["high"].rolling(period).max()
+    lowl = dataframe["low"].rolling(period).min()
     ci = 100 * np.log10((atr.rolling(period).sum()) / (highh - lowl)) / np.log10(period)
     return ci
