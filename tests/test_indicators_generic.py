27c036bd21b8dbe0df336ef47b29255a6d3cc8bd
Matthias <xmatthias@outlook.com>
2024-02-05 19:17:51 +0100

    Improve generic indicator test

--- tests/test_indicators_generic.py
+++ tests/test_indicators_generic.py
@@ -7,42 +7,42 @@ import technical.indicators as ti
 @pytest.mark.parametrize(
     "function,args,responsetype,new_column_names",
     [
-        (ti.atr_percent, [], 'series', ['']),
-        (ti.atr, [], 'series', ['',]),
-        (ti.bollinger_bands, [], 'df', ['',]),
-        (ti.chaikin_money_flow, [], 'series', ['',]),
-        (ti.MADR, [], 'df', ['',]),
+        (ti.atr_percent, [], 'series', None),
+        (ti.atr, [], 'series', None),
+        (ti.bollinger_bands, [], 'df', ['bb_lower', 'bb_middle', 'bb_upper']),
+        (ti.chaikin_money_flow, [], 'series', None),
+        (ti.MADR, [], 'df', ['rate', 'plusdev', 'minusdev', 'stdcenter']),
         (ti.PMAX, [], 'df', ['ATR_10', 'pm_10_3_12_1', 'pmX_10_3_12_1']),
-        (ti.RMI, [], 'series', ['',]),
-        (ti.SSLChannels, [], 'tuple', ['',]),
-        (ti.TKE, [], 'tuple', ['',]),
-        (ti.VIDYA, [], 'series', ['',]),
-        (ti.atr_percent, [], 'series', ['',]),
-        (ti.chaikin_money_flow, [], 'series', ['',]),
-        (ti.chopiness, [], 'series', ['',]),
-        (ti.cmf, [], 'series', ['',]),
-        (ti.ema, [10], 'series', ['',]),
-        (ti.fibonacci_retracements, [], 'series', ['',]),
-        (ti.hull_moving_average, [10], 'series', ['',]),
-        (ti.ichimoku, [], 'dict', ['',]),
-        (ti.laguerre, [], 'list', ['',]),
-        (ti.madrid_sqz, [], 'tuple', ['',]),
-        (ti.mmar, [], 'tuple', ['',]),
-        (ti.osc, [], 'series', ['',]),
-        # (ti.return_on_investment, [], 'series', ['',]),
-        (ti.sma, [10], 'series', ['',]),
-        (ti.stc, [], 'series', ['',]),
-        (ti.td_sequential, [], 'df', ['',]),
-        (ti.dema, [10], 'series', ['',]),
-        (ti.tema, [10], 'series', ['',]),
-        # (ti.tv_hma, [], 'series', ['',]),
-        (ti.tv_wma, [], 'df', ['',]),
-        (ti.vfi, [], 'tuple', ['',]),
-        (ti.vpci, [], 'series', ['',]),
-        (ti.vpcii, [], 'series', ['',]),
-        (ti.vwma, [10], 'series', ['',]),
-        (ti.vwmacd, [], 'df', ['',]),
-        (ti.williams_percent, [], 'series', ['',]),
+        (ti.RMI, [], 'series', None),
+        (ti.SSLChannels, [], 'tuple', None),
+        (ti.TKE, [], 'tuple', None),
+        (ti.VIDYA, [], 'series', None),
+        (ti.atr_percent, [], 'series', None),
+        (ti.chaikin_money_flow, [], 'series', None),
+        (ti.chopiness, [], 'series', None),
+        (ti.cmf, [], 'series', None),
+        (ti.ema, [10], 'series', None),
+        (ti.fibonacci_retracements, [], 'series', None),
+        (ti.hull_moving_average, [10], 'series', None),
+        (ti.ichimoku, [], 'dict', None),
+        (ti.laguerre, [], 'list', None),
+        (ti.madrid_sqz, [], 'tuple', None),
+        (ti.mmar, [], 'tuple', None),
+        (ti.osc, [], 'series', None),
+        # (ti.return_on_investment, [], 'series', None),
+        (ti.sma, [10], 'series', None),
+        (ti.stc, [], 'series', None),
+        (ti.td_sequential, [], 'df', ['TD_count']),
+        (ti.dema, [10], 'series', None),
+        (ti.tema, [10], 'series', None),
+        # (ti.tv_hma, [], 'series', None),
+        (ti.tv_wma, [], 'df', ['tv_wma']),
+        (ti.vfi, [], 'tuple', None),
+        (ti.vpci, [], 'series', None),
+        (ti.vpcii, [], 'series', None),
+        (ti.vwma, [10], 'series', None),
+        (ti.vwmacd, [], 'df', ['', '', '']),
+        (ti.williams_percent, [], 'series', None),
     ],
 )
 def test_indicators_generic_interface(
@@ -75,8 +75,12 @@ def test_indicators_generic_interface(
         # Result is dataframe
         assert isinstance(res, DataFrame)
         assert len(res) == 1000
-        # if (len(new_column_names) > 0):
-        #     assert len(res.columns) == len(new_column_names) + 6
-        #     assert all([x in res.columns for x in new_column_names])
+        if (len(new_column_names) > 0):
+            assert len(res.columns) == len(new_column_names) + 6
+            default_columns = ['date', 'open', 'high', 'low', 'close', 'volume']
+            cols = set(res.columns)
+            assert cols == set(new_column_names + default_columns)
+            # assert set()
+            assert all([x in res.columns for x in new_column_names])
     else:
         assert False
