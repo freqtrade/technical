f098bed6b88879563aa80fc0d446a5c17a2925f3
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2024-09-18 18:01:48 +0900

    fix tv_hma

--- technical/indicators/indicators.py
+++ technical/indicators/indicators.py
@@ -1261,16 +1261,18 @@ def tv_wma(dataframe: DataFrame, length: int = 9, field="close") -> DataFrame:
         dataframe : Pandas DataFrame with new columns 'tv_wma'
     """
 
+    df = dataframe.copy()
+
     norm = 0
     sum = 0
 
     for i in range(1, length - 1):
         weight = (length - i) * length
         norm = norm + weight
-        sum = sum + dataframe[field].shift(i) * weight
+        sum = sum + df[field].shift(i) * weight
 
-    dataframe["tv_wma"] = sum / norm
-    return dataframe
+    df["tv_wma"] = sum / norm if (norm != 0) else 0
+    return df
 
 
 def tv_hma(dataframe: DataFrame, length: int = 9, field="close") -> DataFrame:
@@ -1287,14 +1289,17 @@ def tv_hma(dataframe: DataFrame, length: int = 9, field="close") -> DataFrame:
         dataframe : Pandas DataFrame with new columns 'tv_hma'
     """
 
-    dataframe["h"] = 2 * tv_wma(dataframe, math.floor(length / 2), field) - tv_wma(
-        dataframe, length, field=field
+    df = dataframe.copy()
+
+    df["h"] = (
+        2 * tv_wma(df, math.floor(length / 2), field)["tv_wma"]
+        - tv_wma(df, length, field=field)["tv_wma"]
     )
 
-    dataframe["tv_hma"] = tv_wma(dataframe, math.floor(math.sqrt(length)), field="h")
-    dataframe.drop("h", inplace=True, axis=1)
+    df["tv_hma"] = tv_wma(df, math.floor(math.sqrt(length)), field="h")["tv_wma"]
+    df.drop("h", inplace=True, axis=1)
 
-    return dataframe
+    return df
 
 
 def tv_alma(
